c7058ba088f2ec3bc9a2cc86419f0669a0878edf
war_monger <satyamsharma2106@gmail.com>
2022-07-17 08:09:10 +0530

    add funtionality including changing read status, deleting the book

--- srcipt.js
+++ srcipt.js
@@ -4,10 +4,8 @@ function Book(title, author, pages, whetherRead) {
     this.title = title
     this.author = author
     this.pages = parseInt(pages)
-    this.whetherRead = whetherRead ? 'read already' : 'not yet read';
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${whetherRead}`;
-    }
+    this.whetherRead = whetherRead ? 'read already' : 'not read yet';
+    this.info = () => `${title} by ${author}, ${pages} pages, ${whetherRead}`
 }
 
 function addBookToLibrary() {
@@ -23,12 +21,83 @@ function displayBooks() {
     for (let book of myLibrary) {
         const row = document.createElement('tr');
         for (let prop in book) {
-            const element = document.createElement('td');
-            element.textContent = book.prop;
-            row.appendChild(element);
+            if (typeof book[prop]  === 'function') {
+                continue;
+            }
+            const tableCell = document.createElement('td');
+            tableCell.textContent = book[prop];
+            if (prop === 'whetherRead') {
+                tableCell.classList.add(`book_${myLibrary.indexOf(book)}`);
+            }
+            
+            row.appendChild(tableCell);
         }
+        row.dataset.bookNumber = myLibrary.indexOf(book);
+        row.dataset.readStatus = book.whetherRead;
+
+        const addBookButton = document.createElement('button');
+        addBookButton.classList.add('remove-book');
+        row.appendChild(addBookButton);
+
+        const readStatusButton = document.createElement('button');
+        readStatusButton.classList.add('change-status')
+        row.appendChild(readStatusButton);
+        
         table.appendChild(row);
     }
 }
 
+function getUserInput() {
+    const inputButton = document.querySelector('.input-button');
+    inputButton.addEventListener('click', () => {
+        const inputForm = document.querySelector('form');
+        if (inputForm.style.display === 'none') {
+            inputForm.style.display = 'block';
+        }
+        else {
+            inputForm.style.display = 'none';
+        }
+    });
+    const removeButtonList = document.querySelectorAll('.remove-book');
+    removeButtonList.forEach(removeButton => {
+        removeButton.addEventListener('click', () => {
+            const bookNumber = removeButton.parentElement.dataset.bookNumber;
+            console.log(parseInt(bookNumber));
+            myLibrary.splice(parseInt(bookNumber), 1);
+            removeButton.parentElement.remove();
+    });
+    
+});
+}
+
+
+trialBook = new Book('abd', 'ahf', 80, false);
+trialBook2 = new Book('afuei', 'hoefh', 90, true);
+
+myLibrary.push(trialBook, trialBook2);
+
 displayBooks();
+getUserInput();
+const statusButtonList = document.querySelectorAll('.change-status');
+    statusButtonList.forEach(statusButton => {
+        statusButton.addEventListener('click', () => {
+            console.log(statusButton);
+            const readStatus = statusButton.parentElement.dataset.readStatus;
+            const bookNumber = statusButton.parentElement.dataset.bookNumber;
+            const book = myLibrary[parseInt(bookNumber)];
+            const readStatusCell = document.querySelector(`.book_${myLibrary.indexOf(book)}`);
+            const notRead = 'not read yet';
+            const read = 'read already';
+
+            if (readStatus === 'read already') {
+                statusButton.parentElement.dataset.readStatus = notRead;
+                readStatusCell.textContent = notRead;
+                book.whetherRead = notRead;
+            }
+            else {
+                statusButton.parentElement.dataset.readStatus = read;
+                readStatusCell.textContent = read;
+                book.whetherRead = read;
+            }
+        });
+    });
